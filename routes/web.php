ce53539258fc6a6a3ccc2755a94e7b68e4c4a069
silviansi <silviansi24@gmail.com>
2024-06-18 20:00:56 +0700

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BagianController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\JabatanController;
 use App\Http\Controllers\ManajemenKaryawanController;
+use App\Http\Controllers\StatusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,12 +29,28 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
+    // Route for dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
+    // Route for manajemen karyawan
     Route::get('/manajemen-karyawan', [ManajemenKaryawanController::class, 'index']);
     Route::get('/manajemen-karyawan/create', [ManajemenKaryawanController::class, 'create']);
+    Route::get('/manajemen-karyawan/edit', [ManajemenKaryawanController::class, 'edit']);
 
+    // Route for bagian
     Route::get('/bagian', [BagianController::class, 'index']);
     Route::get('/bagian/create', [BagianController::class, 'create']);
+    Route::get('/bagian/edit', [BagianController::class, 'edit']);
+
+    // Route for jabatan
+    Route::get('/jabatan', [JabatanController::class, 'index']);
+    Route::get('/jabatan/create', [JabatanController::class, 'create']);
+    Route::get('/jabatan/edit', [JabatanController::class, 'edit']);
+
+    // Route for status
+    Route::get('/status', [StatusController::class, 'index']);
+    Route::get('/status/create', [StatusController::class, 'create']);
+    Route::get('/status/edit', [StatusController::class, 'edit']);
+
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
